38009c3a1b67c665dfac62b909fcb720e6d6599c
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-05-15 11:53:46 -0400

    build: prevent node_modules/metro/babel caching issues

--- IntegrationTestingApp/metro.config.js
+++ IntegrationTestingApp/metro.config.js
@@ -6,6 +6,13 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // @babel/runtime@7.29+ uses an array-format exports entry that Metro's
+    // package exports resolver doesn't handle, causing resolution failures
+    // for helpers like slicedToArray even though the files exist on disk.
+    unstable_enablePackageExports: false,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
